d98c56bfa97a6b4ee53d92c89f961fe4aaf57817
vitorfail <vitor_andrademanoel@hotmail.com>
2024-02-14 13:57:41 -0300

    loading

---
+++ src/app/components/Loading/index.js
@@ -0,0 +1,34 @@
+import './index.scss'
+import React from 'react'
+
+/**
+ * @constructor
+ * @param {string} titulo
+ * @param {string} img
+ * @param {string} link 
+ * @param {string} descricao
+ */
+export default function Loading(props){
+    return(
+        <div className={props.show} >
+            <div class="loader">
+                <svg viewBox="0 0 80 80">
+                    <circle id="test" cx="40" cy="40" r="32"></circle>
+                </svg>
+            </div>
+
+            <div class="loader triangle">
+                <svg viewBox="0 0 86 80">
+                    <polygon points="43 8 79 72 7 72"></polygon>
+                </svg>
+            </div>
+
+            <div class="loader">
+                <svg viewBox="0 0 80 80">
+                    <rect x="8" y="8" width="64" height="64"></rect>
+                </svg>
+            </div>
+
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+'use client';
 import Habilidades from './components/Habilidades/Habilidades'
 import Inicial from './components/Inicial/Inicial'
 import Nav from './components/Nav/Nav'
@@ -6,10 +7,19 @@ import Head from 'next/head'
 import "./index.css"
 import Estrelas from './components/Estrelas'
 import Rodape from './components/Rodape/Rodape'
+import { useEffect, useState } from 'react';
+import Loading from './components/Loading';
 
 export default function Home() {
+  const [load, setload] = useState(true)
+  useEffect(() =>{
+    setTimeout(() => {
+      setload(false)
+    }, 2000);
+  }, [])
   return (
     <main>
+      <Loading show={load?"loading show": "loading"} ></Loading>:
       <Estrelas estrelas={[0,0,0,0,0,0,0]} star={[0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]} largura={400} ></Estrelas>
       <Head>
         <title>Vitor Manoel Portifolio</title>
